e31632de385cf2b4295b3fd50cbfda7b6470f27e
Chris Zhang <eimsteim@163.com>
2016-06-29 16:29:34 +0800

    巨大改动：修改了垂直滚动时的排列方式，不局限以div来区分一行，而是通过高度来判断，同时通过虚拟dots解决了用户没有定义dots时的定位问题。

--- jquery.zSlider.js
+++ jquery.zSlider.js
@@ -9,53 +9,67 @@
  */
 (function($){
 
-	$.fn.zSlider = function(options){
-		//default settings
-		var defaults = {
-			// contentWrapper: '',
-			delay: 5000,
-			dotsWrapper: undefined,
+    $.fn.zSlider = function(options){
+        //default settings
+        var defaults = {
+            // contentWrapper: '',
+            delay: 5000,
+            dotsWrapper: undefined,
             align: 'vertical' //x-horizontal, y-vertical
-		};
+        };
 
-		var settings = $.extend(defaults, options);
+        var settings = $.extend(defaults, options);
 
-		//main logical
-		var ul = $(this).find('ul'),
+        //main logical
+        var ul = $(this).find('ul'),
             liH = ul.find('li').outerHeight(true),
             liW = ul.find('li').outerWidth(true),
             child_size = $(ul).children('li').length,
             autoPlay;
 
+        var line_count = ul.outerHeight()/(ul.children('li:first').outerHeight());
+
+        var v_dots_cur, v_dots_max;
+
         /**** 更新dots ****/
         var updateDots = function(tar_index) {
 
-            if(settings.dotswrapper){
-                var dots = $(settings.dotswrapper).find('.dots');
+            if(settings.dotsWrapper){
+                var dots = $(settings.dotsWrapper).find('.dots');
                 $(dots).find('li.active').removeClass('active');
                 $($(dots).children('li').get(tar_index)).addClass('active');
             }
         }
 
         /**** 滚动到指定target ****/
-		var scrollTo = function(target) {
-			//fresh li's active status
-            var curr = $(ul).find('li.active'); $(curr).removeClass('active');
-            var tar_index;
-            if(target) {
-                tar_index = $(target).index();
-                $($(ul).children('li').get(tar_index)).addClass('active');
-            } else {
-                var next = curr.next();
-                if($(next).index() > 0){
-                    tar_index = $(next).addClass('active').index();
-                } else {
-                    tar_index = 0; //back to first li
-                    $(ul).find('li:first').addClass('active');
-                }
-            }
+        //然而这里的target在水平切换时标识一个 li
+        //而在垂直切换时标识一个 line
+        var scrollTo = function(target) {
 
             if(settings.align == 'horizontal') { //horizontal
+
+                //fresh li's active status
+                var curr = $(ul).find('li.active');
+
+                if(curr.length > 0) {
+                    $(curr).removeClass('active');
+                } else {
+                    curr = $(ul).find('li:first');
+                }
+
+                var tar_index;
+                if(target) {
+                    tar_index = $(target).index();
+                    $($(ul).children('li').get(tar_index)).addClass('active');
+                } else {
+                    var next = curr.next();
+                    if($(next).index() > 0){
+                        tar_index = $(next).addClass('active').index();
+                    } else {
+                        tar_index = 0; //back to first li
+                        $(ul).find('li:first').addClass('active');
+                    }
+                }
 
                 //adjust ul's width
                 if(child_size > 0){
@@ -71,17 +85,45 @@
                 });
 
             } else { //vertical
+
+                //target == line
+                //此时根据dots的index来切换（依赖于dots和content的同步性）
+
+                var next_index;
+                if(settings.dotsWrapper) {
+                    //如果用户定义了dots
+                    var next = $(settings.dotsWrapper).find('.dots>.dot.active').next();
+                    if(next.length > 0) {
+                        //如果有下一个
+                        next_index = next.index();
+                    } else {
+                        //如果没有下一个，切换到第一个
+                        next_index = 0;
+                    }
+
+                } else {
+                    //如果用户没有使用dots
+                    //v_dots_max: 虚拟dots，最大值
+                    //v_dots_cur: 虚拟dots，当前值
+                    if(v_dots_cur < v_dots_max) {
+                        next_index = v_dots_cur++;
+                    } else {
+                        next_index = 0;
+                        v_dots_cur = 0;
+                    }
+                }
+
                 ul.animate({
-                    'margin-top':-liH * tar_index +'px'
+                    'margin-top':-liH * next_index +'px'
                 },500,function(){
                     //update dots
-                    updateDots(tar_index);
+                    updateDots(next_index);
                 });
             }
-		}
+        }
 
         $(this).hover(function(){
-        	clearInterval(autoPlay);
+            clearInterval(autoPlay);
         },function(){
             autoPlay=setInterval(function(){
                 scrollTo();
@@ -89,14 +131,22 @@
         }).trigger('mouseleave');
 
         //init dots
-        if(settings.dotswrapper){
-        	
-            $(settings.dotswrapper).append('<ol class="dots"></ol>');
-            var dots = $(settings.dotswrapper).find('.dots');
+        if(settings.dotsWrapper){
 
-            $.each(ul.children('li'), function(i, n){
-                $(dots).append('<li class="dot"></li>');
-            });
+            $(settings.dotsWrapper).append('<ol class="dots"></ol>');
+            var dots = $(settings.dotsWrapper).find('.dots');
+
+            if(settings.align == 'horizontal') {
+                $.each(ul.children('li'), function(i, n){
+                    $(dots).append('<li class="dot"></li>');
+                });
+            } else {
+                //获取ul高度
+                for(var i=0; i< line_count; i++){
+                    $(dots).append('<li class="dot"></li>');
+                }
+            }
+
             $(dots).find('li:first').addClass('active');
             //bind event
             $(dots).find('li').on('click', function(){
@@ -110,10 +160,18 @@
 //                    scrollTo();
 //                }, settings.delay?settings.delay:5000);
             });
+        } else {
+            //初始化虚拟dots
+            v_dots_cur = 0;
+            if(settings.align == 'horizontal') {
+                v_dots_max = ul.children('li').length;
+            } else {
+                //获取ul高度
+                v_dots_max = line_count;
+            }
         }
-		
 
-		return this;
-	}
+        return this;
+    }
 
 })(jQuery);
